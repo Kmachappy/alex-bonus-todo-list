8c9cde0c20a43d072f38c36f083a768288cd766e
Alejandro Rojas <kmachappy@pm.me>
2022-04-23 12:14:20 -0700

    listing todos

--- server.js
+++ server.js
@@ -18,8 +18,25 @@ app.use(morgan("dev")) // log http requests and codes
 
 
 // routes
-app.get("/",(req,res)=>{
-    res.send("hello")
+app.get("/", async (req,res)=>{
+    // go get todos
+    const todos = await Todo.find({})
+    
+    // render index.ejs
+    res.render("index.ejs", {todos})
+})
+
+app.get("/todo/seed", async (req,res)=>{
+    // delete all existing
+    await Todo.remove({}).catch((err)=> res.send(err))
+    // add your sample todos
+    const todos = await Todo.create([
+        {text: "eat breakfast", completed: false},
+        {text: "eat lunch", completed:false},
+        {text: "eat dinner", completed: false}
+    ]).catch((err)=>res.send(err))
+    // send the todos as json
+    res.json(todos)
 })
 
 
@@ -38,7 +55,13 @@ db
 .on("connected", ()=> console.log("Mongo DB Connected"))
 .on("disconnected", ()=> console.log("Mongo DB Disconnected"))
 
+// schemas and models
+const todoSchema = new mongoose.Schema({
+    text: String,
+    completed: Boolean,
+}, {timestamps: true})
 
+const Todo = mongoose.model("Todo", todoSchema)
 
 
 // server listener
